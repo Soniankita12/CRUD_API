3480d4bdd305b7cb8316c6dd81c3e5f44d35407e
soniankita12 <ankitasoni2211104@gmail.com>
2024-03-22 17:37:11 +0530

    routing

---
+++ Routes/ProductRoute.js
@@ -0,0 +1,74 @@
+const express =require("express");
+const router =express.Router();
+const Product = require("../Model/ProductModel");
+
+router.post("/product", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Read data (all data);
+
+router.get("/product", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Read data by id
+
+router.get("/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//update data
+
+router.put("/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      return res.status(404).json({ message: "No product found" });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Delete data
+
+router.delete("/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: "Can't find any product assosiated with this id" });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports =router ;
--- Server.js
+++ Server.js
@@ -1,11 +1,19 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const Product = require("./Model/ProductModel");
-const cors =require("cors");
-const port =process.env.PORT || 3000;
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+const ProductRoute = require('./Routes/ProductRoute');
+
 const app = express();
 app.use(cors());
+
+const MONGO_URL = process.env.MONGO_URL;
 app.use(express.json());
+//routes
+
+app.use('/api',ProductRoute)
+
 app.get("/", (req, res) => {
   res.send("hello nodeji");
 });
@@ -16,85 +24,15 @@ app.get("/create", (req, res) => {
 
 // Create data
 
-app.post("/product", async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: error.message });
-  }
-});
-
-//Read data (all data);
-
-app.get("/product", async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json(products);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// Read data by id
-
-app.get("/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.status(200).json(product);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: error.message });
-  }
-});
-
-//update data
-
-app.put("/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body);
-    if (!product) {
-      return res.status(404).json({ message: "No product found" });
-    }
-    res.status(200).json(product);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: error.message });
-  }
-});
-
-//Delete data
-
-app.delete("/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndDelete(id);
-    if(!product)
-    {
-      return res.status(404).json({message:"Can't find any product assosiated with this id"});
-    }
-  res.status(200).json(product);
-  } catch (error) {
-    console.log(erroe.message);
-    res.status(500).json({message:error.message});
-  }
-});
-
-app.listen(port, () => {
-  console.log("node is running");
-});
 
 
 mongoose
-  .connect(
-    "mongodb+srv://Ankita:Ankita@cluster0.6lbx3ge.mongodb.net/Node-Api?retryWrites=true&w=majority&appName=Cluster0"
-  )
+  .connect(MONGO_URL)
   .then(() => {
     console.log("mongodb is connected");
+    app.listen(port, () => {
+      console.log(`node is running on ${port}`);
+    });
   })
   .catch((error) => {
     console.log(error);
